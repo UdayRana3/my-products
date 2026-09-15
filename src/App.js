aa7ab8ebeeece51dc08e1bb0e5a72380cb632da5
Uday Rana <uday00610@gmail.com>
2026-09-15 23:24:06 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          know react
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const items = [
+  {
+    name: "Samsung S24",
+    price: 127800,
+    image: "images/image2.webp",
+  },
+  {
+    name: "Watch",
+    price: 2450,
+    image: "images/item1.jpg",
+  },
+  {
+    name: "Headphones",
+    price: 1900,
+    image: "images/item3.webp",
+  },
+  {
+    name: "Book",
+    price: 650,
+    image: "images/item4.jpg",
+  },
+  {
+    name: "Mike",
+    price: 3900,
+    image: "images/item5.jpg",
+  },
+  {
+    name: "Samsung Galaxy",
+    price: 18700,
+    image: "images/item6.jpg",
+  },
+  {
+    name: "Ear Buds",
+    price: 3200,
+    image: "images/item7.jpg",
+  },
+];
